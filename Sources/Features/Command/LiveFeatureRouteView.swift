53d14dbe46dc58fa03e6d042e69029262716d08c
matthayesego-web <matt.hayesego@gmail.com>
2026-09-06 03:56:23 -0400

    Route community features to live iOS screens

---
+++ Sources/Features/Command/LiveFeatureRouteView.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct LiveFeatureRouteView: View {
+    let route: FeatureRoute
+    let session: AuthSession
+
+    var body: some View {
+        switch route {
+        case .factionChat:
+            FactionChatLiveView(session: session)
+        case .training:
+            TrainingCenterLiveView(session: session)
+        case .resources:
+            FactionResourcesLiveView(session: session)
+        default:
+            FeatureRouteView(route: route, session: session)
+        }
+    }
+}
